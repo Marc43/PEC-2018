442d23c9c321d9c101777a72dd1a02ad14ce89b0
pec02 <pec02>
2018-03-06 15:58:47 +0100

    Initial ProcesadorSenzill commit

---
+++ ProcesadorSenzill/regfile.vhd
@@ -0,0 +1,31 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all; --Esta libreria sera necesaria si usais conversiones CONV_INTEGER
+
+ENTITY regfile IS
+    PORT (clk    : IN  STD_LOGIC;
+          wrd    : IN  STD_LOGIC;
+          d      : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          addr_a : IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
+          addr_d : IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
+          a      : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
+END regfile;
+
+
+ARCHITECTURE Structure OF regfile IS
+	type register_set is array (7 downto 0) of std_logic_vector(15 downto 0); -- 8 registres de 16 bits cadascun
+	
+	SIGNAL regs : register_set;
+BEGIN
+
+	PROCESS (clk)
+	BEGIN
+	
+		IF rising_edge(clk) AND wrd = '1' THEN
+			regs(conv_integer(addr_a)) <= d;
+		END IF;
+	
+	END PROCESS;
+	a <= regs(conv_integer(addr_a)); -- El valor de sortida (lectura) es sempre el registre indicat
+	 
+END Structure;
