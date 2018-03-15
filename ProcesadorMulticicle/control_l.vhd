594735fbbe22521654fc58027926f52e5ba72725
pec02 <pec02>
2018-03-15 14:08:28 +0100

    L. Control acabada

--- ProcesadorMulticicle/control_l.vhd
+++ ProcesadorMulticicle/control_l.vhd
@@ -1,5 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
 
 ENTITY control_l IS
     PORT (ir        : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
@@ -16,10 +17,76 @@ ENTITY control_l IS
           word_byte : OUT STD_LOGIC);
 END control_l;
 
-
 ARCHITECTURE Structure OF control_l IS
+	-- Alu operations
+	CONSTANT MOVI	: STD_LOGIC_VECTOR(1 DOWNTO 0) := "00";
+	CONSTANT MOVHI	: STD_LOGIC_VECTOR(1 DOWNTO 0) := "01";
+	CONSTANT ADD	: STD_LOGIC_VECTOR(1 DOWNTO 0) := "10";
+
+	-- Op. codes
+	CONSTANT MOV 	: STD_LOGIC_VECTOR (3 DOWNTO 0) := "0101";
+	CONSTANT LD 	: STD_LOGIC_VECTOR (3 DOWNTO 0) := "0011";
+	CONSTANT ST 	: STD_LOGIC_VECTOR (3 DOWNTO 0) := "0100";
+	CONSTANT LDB 	: STD_LOGIC_VECTOR (3 DOWNTO 0) := "1101";
+	CONSTANT STB 	: STD_LOGIC_VECTOR (3 DOWNTO 0) := "1110";
+	CONSTANT HALT	: STD_LOGIC_VECTOR (3 DOWNTO 0) := "1111"; -- By the moment stands for HALT
+	
+	SIGNAL op_code 	: STD_LOGIC_VECTOR (3 DOWNTO 0);
+	SIGNAL reg_src1	: STD_LOGIC_VECTOR (2 DOWNTO 0);
+	SIGNAL reg_src2	: STD_LOGIC_VECTOR (2 DOWNTO 0);
+	
+	SIGNAL immed_ma	: STD_LOGIC_VECTOR (5 DOWNTO 0);
+	SIGNAL immed_alu	: STD_LOGIC_VECTOR (7 DOWNTO 0);
+	
 BEGIN
 
-    -- Aqui iria la generacion de las senales de control del datapath
+	op_code 		<= ir(15 DOWNTO 12);
+	reg_src1		<= ir(11 DOWNTO 9);
+	reg_src2		<=	ir(8	DOWNTO 6);
+	
+	addr_a 	<= reg_src2; -- Constant for every LDX STX instruction
+	addr_b 	<= reg_src1;
+	addr_d 	<= reg_src1;
+	
+	immed_ma 	<= ir(5 DOWNTO 0);
+	immed_alu	<= ir(7 DOWNTO 0);
+	
+	immed			<= std_logic_vector(resize(unsigned(immed_ma), immed'length)) 	WHEN 		op_code = LD 	OR
+																													op_code = LDB 	OR
+																													op_code = ST 	OR
+																													op_code = STB	ELSE
+					
+						std_logic_vector(resize(unsigned(immed_alu), immed'length)) WHEN 		op_code = MOV;
 
+	op		<= ADD 	WHEN 		op_code = LD	OR 
+									op_code = ST	OR 
+									op_code = LDB	OR 
+									op_code = STB 	ELSE
+				
+				MOVI 	WHEN		op_code = MOV 	AND
+									ir(8) = '0' 	ELSE 	-- MOVI
+				
+				MOVHI WHEN		op_code = MOV 	AND 
+									ir(8) = '1'; 			-- MOVHI
+				
+	ldpc	<=	'0' 	WHEN op_code = HALT ELSE
+				'1';
+	
+	
+	wr_m	<= '1' 	WHEN op_code = ST 	OR 
+							  op_code = STB 	ELSE		
+				'0'; 
+				
+	in_d	<= '1' 	WHEN op_code = LD 	OR
+							  op_code = LDB 	ELSE
+				'0';
+				
+	immed_x2 	<= '1' WHEN op_code = LD OR
+									op_code = ST ELSE		
+						'0';
+					
+	word_byte 	<= '1' WHEN op_code = LDB OR
+									op_code = STB ELSE		
+						'0'; -- Note that could be "NOT immed_x2"
+	
 END Structure;
