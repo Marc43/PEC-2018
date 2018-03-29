2ec33edd1ebd9719357894a8a85e07474826e245
Marc <marcdominguezr@gmail.com>
2018-03-29 19:29:57 +0200

    Grafo de estados hecho, test_mem empezado

--- ProcesadorMulticicleMemoria/SRAMController.vhd
+++ ProcesadorMulticicleMemoria/SRAMController.vhd
@@ -22,33 +22,59 @@ end SRAMController;
 
 architecture comportament of SRAMController is                 
 	
-	TYPE states_t IS (IDLE, RD, WR); 
+	TYPE states_t IS (IDLE_ST, RD_ST, WR_ST); 
 	
-	SIGNAL state : states_t := IDLE;
+	SIGNAL state : states_t := IDLE_ST;
 
 begin
 
-		estado: PROCESS (clk)
+		estado: PROCESS (clk) -- Calculates the state to jump
 		BEGIN
 		
 			IF rising_edge(clk) THEN
 			
 				CASE state IS
-					WHEN IDLE =>
+					WHEN IDLE_ST =>
 						IF WR = '1' THEN
-							state <= WR;
+							state <= WR_ST;
 						ELSE
-							state <= RD;
+							state <= RD_ST; 
 						END IF;
-					WHEN RD =>
-						state <= IDLE;
-					WHEN WR =>
-						state <= IDLE;
+					WHEN RD_ST =>
+						state <= IDLE_ST;
+					WHEN WR_ST =>
+						state <= IDLE_ST;
 				END CASE;
 			END IF;
 		END PROCESS;
+		
+		senyales: PROCESS (clk) -- Checks the actual state and assigns the signals for the jumping state
+		BEGIN
+		  
+		  IF rising_edge(clk) THEN
+		    
+    		    CASE state IS
+		        WHEN WR_ST | RD_ST => -- goes to IDLE (ensures that writes nor reads are performed)
+		          SRAM_DQ <= "XXXXXXXXXXXXXXXX"; 
+		          SRAM_OE_N <= '1'; -- Output not enabled 
+		          SRAM_CE_N <= '1'; -- Chip input not enabled
+		          SRAM_WE_N <= '1'; -- Write not enabled
+		        WHEN IDLE_ST =>
+		          IF WR = '0' THEN -- goes to RD
+		             SRAM_OE_N <= '0';
+		             SRAM_CE_N <= '0';
+		             SRAM_UB_N <= '0'; 
+		             SRAM_LB_N <= '0'; 
+		          --ELSE THEN -- goes to WR
+		            
+		            -- TODO
+		            
+		          END IF;
+		       END CASE;
+		    END IF;
+		  
+		END PROCESS;
 			
-		SRAM_ADDR 	<= address;
-
+		SRAM_ADDR 	<= "00" & address;
 
 end comportament;
