83726e3e323ceaf9d95453d9eae316c92c5e04cf
pec02 <pec02>
2018-04-13 15:55:11 +1100

    Teclat funcionant

--- ProcesadorMulticicleIO/controladoresIO.vhd
+++ ProcesadorMulticicleIO/controladoresIO.vhd
@@ -11,6 +11,8 @@ ENTITY controladores_IO IS
 			rd_io 		: out std_logic_vector(15 downto 0); 
 			wr_out 		: in  std_logic; 
 			rd_in 		: in  std_logic; 
+			ps2_clk 		: inout std_logic;
+			ps2_data		: inout std_logic;
 			led_verdes  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0); 
 			led_rojos   : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
 			display		: OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
@@ -21,7 +23,7 @@ END controladores_IO;
 
 ARCHITECTURE Structure OF controladores_IO IS
 	
-	type IO_address_space is array (255 downto 0) of std_logic_vector (15 downto 0);
+	type IO_address_space is array (22 downto 0) of std_logic_vector (15 downto 0); --Perque compili mes rapid.
 	
 	SIGNAL ports : IO_address_space;
 
@@ -31,28 +33,62 @@ ARCHITECTURE Structure OF controladores_IO IS
 	CONSTANT SWITCH_P				: STD_LOGIC_VECTOR (7 DOWNTO 0) := X"08";
 	CONSTANT POWER_DISPLAY_P 	: STD_LOGIC_VECTOR (7 DOWNTO 0) := X"09";	
 	CONSTANT DISPLAY_P 			: STD_LOGIC_VECTOR (7 DOWNTO 0) := X"0A"; 
+	CONSTANT KEYBOARD_READ_P	: STD_LOGIC_VECTOR (7 DOWNTO 0) := X"0F";
+	CONSTANT KEYBOARD_ST_P		: STD_LOGIC_VECTOR (7 DOWNTO 0) := X"10";
+	COMPONENT keyboard_controller is
+   Port (clk        : in    STD_LOGIC;
+          reset      : in    STD_LOGIC;
+          ps2_clk    : inout STD_LOGIC;
+          ps2_data   : inout STD_LOGIC;
+          read_char  : out   STD_LOGIC_VECTOR (7 downto 0);
+          clear_char : in    STD_LOGIC;
+          data_ready : out   STD_LOGIC);
+	END COMPONENT;
+	
+	signal read_char_bus : STD_LOGIC_VECTOR (7 downto 0);
+	signal clear_char_bus : STD_LOGIC := '0' ;
+	signal data_ready_bus : STD_LOGIC := '0' ;
 	
 BEGIN
 
+		keyboard_controller0 : keyboard_controller
+		port map(
+			clk => CLOCK_50,
+			reset => boot,
+			ps2_clk => ps2_clk,
+			ps2_data => ps2_data,
+			read_char =>read_char_bus,
+			clear_char=> clear_char_bus,
+			data_ready => data_ready_bus
+		);
+
 		led_verdes <= ports(conv_integer(LEDV_P))(7 DOWNTO 0);  -- port 5
 		led_rojos  <= ports(conv_integer(LEDR_P))(7 DOWNTO 0);  -- port 6
-
+		
 		display 			<= ports(conv_integer(DISPLAY_P));
 		power_display 	<= ports(conv_integer(POWER_DISPLAY_P))(3 DOWNTO 0);
 		
-		rd_io	<= ports(conv_integer(addr_io)) WHEN rd_in 	= '1'; -- Reads
+		rd_io	<= X"000" & "000" & data_ready_bus WHEN rd_in = '1' AND addr_io = KEYBOARD_ST_P ELSE
+					X"00" & read_char_bus	WHEN rd_in = '1' AND addr_io = KEYBOARD_READ_P ELSE
+					ports(conv_integer(addr_io)) WHEN rd_in = '1';  -- Reads
 		
 		PROCESS (CLOCK_50)
 		BEGIN
-		
+			
+			
 			IF rising_edge(CLOCK_50) THEN
-				IF wr_out 	= '1' THEN
-					ports(conv_integer(addr_io)) <= wr_io; -- Writes
+				IF clear_char_bus = '1' THEN
+					clear_char_bus <= '0';
+				END IF;
+				
+				IF wr_out = '1' AND addr_io /= KEYBOARD_ST_P THEN
+					ports(conv_integer(addr_io)) <= wr_io ; -- Writes
+				ELSIF wr_out = '1' THEN
+					clear_char_bus <= '1';
 				END IF;
 				
 				ports(conv_integer(KEY_P)) 	<= X"000" & keys;
 				ports(conv_integer(SWITCH_P)) <=  X"00" & switches;
-				
 			END IF;
 		
 		END PROCESS;
