1e315cd71fe35dcea25bb334ca7502e4ec68b4b3
Aleix Sanchis <aleixsanchis@gmail.com>
2018-05-21 13:26:52 +0200

    Añadido control del flush

--- ProcesadorMulticicleTLB/tlb.vhd
+++ ProcesadorMulticicleTLB/tlb.vhd
@@ -1,25 +1,25 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
-USE ieee.std_logic_unsigned.all; 
 USE ieee.numeric_std.all;        
 
 ENTITY TLB IS
 
 	PORT(
 		clk: 			IN STD_LOGIC;
-		vtag: 		IN STD_LOGIC_VECTOR(3 downto 0);
-		ptag_wr:		IN STD_LOGIC_VECTOR(3 downto 0);
-		vtag_wrd:	IN STD_LOGIC;
-		ptag_wrd:	IN STD_LOGIC;
-		boot:			IN STD_LOGIC; 
-		valid_wr: 	IN STD_LOGIC;
-		read_only_wr:		IN STD_LOGIC;
-		wr_table_entry:	IN STD_LOGIC_VECTOR(3 downto 0);	
+		vtag: 		IN STD_LOGIC_VECTOR(3 downto 0); --Tag Virtual a leer o escribir
+		ptag_wr:		IN STD_LOGIC_VECTOR(3 downto 0);	--Tag Fisico a escribir
+		vtag_wrd:	IN STD_LOGIC;	--Permiso de escritura del Tag Virtual
+		ptag_wrd:	IN STD_LOGIC;	--Permiso de escritura del Tag Fisico
+		boot:			IN STD_LOGIC; 	
+		valid_wr: 	IN STD_LOGIC;	--Senyal de validez a escribir en el nuevo tag fisico
+		read_only_wr:		IN STD_LOGIC; --Senyal de read_only a escribir en el nuevo tag fisico
+		wr_table_entry:	IN STD_LOGIC_VECTOR(3 downto 0); --Entrada de la tabla a escribir
+		flush:				IN STD_LOGIC; --Si hace falta flushear el TLB
 		
-		ptag_rd:			OUT STD_LOGIC_VECTOR(3 downto 0);
-		valid_rd:		OUT STD_LOGIC;
-		read_only_rd:	OUT STD_LOGIC;
-		hit:				OUT STD_LOGIC
+		ptag_rd:			OUT STD_LOGIC_VECTOR(3 downto 0); --Tag Fisico eido del TLB
+		valid_rd:		OUT STD_LOGIC;	--Senyal de validez del tag Fisico leido del TLB
+		read_only_rd:	OUT STD_LOGIC;	--Senyal de read_only del tag Fisico leido del TLB
+		hit:				OUT STD_LOGIC	--'1' si ha habido acierto de TLB, 0 si no
 	);
 	
 END TLB;
@@ -43,10 +43,17 @@ ARCHITECTURE Structure of TLB IS
 			IF boot='1' THEN
 				--Inicialitzar entrades TLB?
 			ELSIF rising_edge(clk) THEN
-				IF vtag_wrd = '1' THEN --Escriptura al TAG VIRTUAL
-					vtags(CONV_INTEGER(wr_table_entry)) <= vtag;
+				IF flush='1' THEN
+					ptags <= (others => (others => '0'));
+					--No estoy seguro de esto de arriba. La documentacion no lo especifica mucho.
+					--No se si deberia provocar misses de TLB o fallos de TLB. De la forma que esta
+					--Ahora provocara fallos de TLB. Supongo que mal no esta. No sabia como cambiar
+					--Solo el bit de validez de cada entrada asi que lo pongo todo a cero
+					
+				ELSIF vtag_wrd = '1' THEN --Escriptura al TAG VIRTUAL
+					vtags(to_integer(unsigned(wr_table_entry))) <= vtag;
 				ELSIF ptag_wrd = '1' THEN--Escriptura al TAG FISIC		
-					ptags(CONV_INTEGER(wr_table_entry)) <= valid_wr & read_only_wr & ptag_wr;
+					ptags(to_integer(unsigned(wr_table_entry))) <= valid_wr & read_only_wr & ptag_wr;
 				ELSE	--Lectura
 					valid_rd <= '0';
 					read_only_rd <= '1';
